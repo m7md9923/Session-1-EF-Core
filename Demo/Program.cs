4ac4d5d9ad3a766ad7d489bce64cfb210d96c3ff
m7md9923 <mhmdnsrbrkat0@gmail.com>
2025-08-27 20:57:05 +0300

    update

---
+++ Demo/Program.cs
@@ -0,0 +1,55 @@
+﻿using Demo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Demo
+
+        // More than one DbContext ==> connect more than one DB
+        // APIS ==> Connects 2 DB ==> Identity [UserInfo-Roles] -- Business
+        // IndentityDbContext, BusinessDbContext
+        // Class DbContext must end with keyword DbContext
+        // DbContext ==> CompanyDbContext 
+        
+        MyCompanyDbContext dbContext = new();
+        
+        // Old way 
+        
+        // try
+        // {
+        //     // Some Code
+        //     
+        // }
+        // finally  // keda keda excuted even if exception happened
+        // {
+        //     dbContext.Dispose(); // Close connection
+        // }
+
+        // old
+        
+        // using (dbContext)
+        // {
+        //     // Some Code
+        //     
+        // }
+
+
+        //using (dbContext);  // most common used 
+        // Some Code
+        // dbContext.Database.Migrate();
+        
+        
+        
+        
+        
+        
+
+
+        #endregion
+    }
+    
+}
